ef12c45cc211a8bcaf5497424dac5fcbf5dee5a7
Varmen8 <Varmen8@users.noreply.github.com>
2019-04-08 00:39:06 +0200

    Add user menu prototype

--- pozzie/src/app/app.module.ts
+++ pozzie/src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { 
+  MatToolbarModule,
+   MatButtonModule, 
+   MatSidenavModule, 
+   MatIconModule, 
+   MatListModule,
+   MatCardModule,
+   MatExpansionModule,
+   MatProgressBarModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import { UserMenuComponent } from './user-menu/user-menu.component';
+
+import { ProfileComponent } from './profile/profile.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+    {
+        path: 'profile',
+        component: ProfileComponent
+    }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserMenuComponent,
+    ProfileComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
