ec19c3d95a474d3faa3890415bf9a07d455b2835
Varmen8 <Varmen8@users.noreply.github.com>
2019-04-08 00:39:06 +0200

    Add user menu prototype

---
+++ pozzie/src/app/user-menu/user-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-user-menu',
+  templateUrl: './user-menu.component.html',
+  styleUrls: ['./user-menu.component.css']
+})
+export class UserMenuComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
